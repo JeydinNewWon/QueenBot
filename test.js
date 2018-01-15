9de9e82be3aa83405e99bb4596da7ae82fe18fd2
JeydinNewWon <jayden.nguyen@sydstu.catholic.edu.au>
2018-01-15 14:40:49 +1100

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const jsonfile = require('jsonfile');
-
-jsonfile.readFile('/Users/jayden.nguyen/Desktop/MyBot/data/queens/385379974951206913.json', (err, obj) => {
-    obj['stampy'].push('blah');
-    jsonfile.writeFile('/Users/jayden.nguyen/Desktop/MyBot/data/queens/385379974951206913.json', obj, { spaces: 4 }, (err) => {
-
-    });
-});
